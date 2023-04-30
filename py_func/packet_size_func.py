c73b7583ff74cd48ae4966a87adff938d1052476
VadAxel <58472614+VadAxel@users.noreply.github.com>
2023-04-30 12:59:51 +0200

    Update packet_size_func.py
    Cleaner

--- py_func/packet_size_func.py
+++ py_func/packet_size_func.py
@@ -3,19 +3,14 @@
 ########################################
 
 def main_func(capture, protocols, file_count, conversation_lengths):
+    total_traffic = 0
     for packet in capture:
 
-    # Increment the count for the protocol used in this packet
-        if packet.highest_layer in protocols:
-            protocols[packet.highest_layer] += 1
-        else:
-            protocols[packet.highest_layer] = 1
-
-    # Check if this packet has a file attached to it
+        # Check if this packet has a file attached to it
         if 'http' in packet and packet.http.get_field('response_for_uri') is not None:
             file_count += 1
 
-    # Increment the count for the source and destination IP addresses and ports
+        # Increment the count for the source and destination IP addresses and ports
         if 'ip' in packet:
             src_ip = packet.ip.src
             dst_ip = packet.ip.dst
@@ -34,10 +29,10 @@ def main_func(capture, protocols, file_count, conversation_lengths):
                 src_port = packet.tcp.srcport
                 dst_port = packet.tcp.dstport
 
-        # Calculate the size of the data payload
             payload_size = int(packet.captured_length)
 
-        # Check the direction of the traffic and increment the appropriate entry in the conversation_lengths dictionary
+            total_traffic += payload_size
+
             if (src_ip, dst_ip, src_port, dst_port) in conversation_lengths:
                 conversation_lengths[(src_ip, dst_ip, src_port, dst_port)][0] += payload_size
             else:
@@ -47,3 +42,6 @@ def main_func(capture, protocols, file_count, conversation_lengths):
                 conversation_lengths[(dst_ip, src_ip, dst_port, src_port)][1] += payload_size
             else:
                 conversation_lengths[(dst_ip, src_ip, dst_port, src_port)] = [0, payload_size]
+
+    return total_traffic
+
